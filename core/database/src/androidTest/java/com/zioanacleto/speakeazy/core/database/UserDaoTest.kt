a7343de83bb1472f93f11c78af17514449110e30
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2026-01-25 01:15:41 +0100

    Add more android tests

--- core/database/src/androidTest/java/com/zioanacleto/speakeazy/core/database/UserDaoTest.kt
+++ core/database/src/androidTest/java/com/zioanacleto/speakeazy/core/database/UserDaoTest.kt
@@ -1,5 +1,42 @@
 package com.zioanacleto.speakeazy.core.database
 
+import com.zioanacleto.speakeazy.core.database.entities.UserEntity
+import org.junit.Test
+
 class UserDaoTest : AbstractDatabaseTest() {
 
+    @Test
+    fun test_getUserByEmail_returnsUserWhenItExists() {
+        // given
+        insertUsers()
+        val email = "test@test.com"
+
+        // when
+        val user = userDao.getUserByEmail(email)
+
+        // then
+        assert(user?.email == email)
+    }
+
+    @Test
+    fun test_getUserByEmail_returnsNullWhenUserDoesNotExist() {
+        // given
+        insertUsers()
+        val email = "notexisting@test.com"
+
+        // when
+        val user = userDao.getUserByEmail(email)
+
+        // then
+        assert(user == null)
+    }
+
+    private fun insertUsers() {
+        userDao.insertUser(
+            UserEntity(
+                name = "John Doe",
+                email = "test@test.com"
+            )
+        )
+    }
 }
