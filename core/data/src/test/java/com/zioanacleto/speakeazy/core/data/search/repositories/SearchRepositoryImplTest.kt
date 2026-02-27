58fd177b93ce020fabbdc2bfe3e22c4163135635
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2026-02-27 18:55:16 +0100

    Add SearchRepositoryImplTest

--- core/data/src/test/java/com/zioanacleto/speakeazy/core/data/search/repositories/SearchRepositoryImplTest.kt
+++ core/data/src/test/java/com/zioanacleto/speakeazy/core/data/search/repositories/SearchRepositoryImplTest.kt
@@ -4,13 +4,17 @@ import com.zioanacleto.buffa.coroutines.DefaultDispatcherProvider
 import com.zioanacleto.buffa.coroutines.DispatcherProvider
 import com.zioanacleto.buffa.events.Resource
 import com.zioanacleto.speakeazy.core.analytics.traces.PerformanceTracesManager
+import com.zioanacleto.speakeazy.core.data.assertAllTrue
 import com.zioanacleto.speakeazy.core.data.detail.datasources.IngredientDataSource
 import com.zioanacleto.speakeazy.core.data.search.datasources.SearchDataSource
 import com.zioanacleto.speakeazy.core.data.search.datasources.SearchQueriesDataSource
+import com.zioanacleto.speakeazy.core.domain.detail.model.IngredientsModel
 import com.zioanacleto.speakeazy.core.domain.main.model.MainModel
 import com.zioanacleto.speakeazy.core.domain.search.model.SearchModel
+import com.zioanacleto.speakeazy.core.domain.search.model.TagsModel
 import io.mockk.clearAllMocks
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
@@ -47,7 +51,7 @@ class SearchRepositoryImplTest {
     }
 
     @Test
-    fun test_submitQuery() = runBlocking {
+    fun `submitQuery - returns Success`() = runBlocking {
         // given
         coEvery { searchDataSource.querySearch(any(), any()) } returns Resource.Success(
             SearchModel()
@@ -62,7 +66,83 @@ class SearchRepositoryImplTest {
     }
 
     @Test
-    fun test_submitFilter() = runBlocking {
+    fun `getSearchLandingData - when ingredients and tags are OK return success`() = runBlocking {
+        // given
+        coEvery {
+            ingredientDataSource.getIngredientsList()
+        } returns Resource.Success(
+            IngredientsModel(listOf())
+        )
+        coEvery {
+            searchDataSource.getTags()
+        } returns Resource.Success(
+            TagsModel(listOf())
+        )
+        coEvery {
+            searchQueriesDataSource.getLastQueries()
+        } returns Resource.Success(listOf())
+
+        // when
+        val sut = createSut()
+        val response = sut.getSearchLandingData().first()
+
+        // then
+        assertAllTrue(
+            response is Resource.Success,
+            (response as Resource.Success).data.tags.isEmpty(),
+            response.data.ingredients.isEmpty(),
+            response.data.lastQueries.isEmpty(),
+        )
+    }
+
+    @Test
+    fun `getSearchLandingData - when ingredients is KO return Error`() = runBlocking {
+        // given
+        coEvery {
+            ingredientDataSource.getIngredientsList()
+        } returns Resource.Error(Exception())
+        coEvery {
+            searchDataSource.getTags()
+        } returns Resource.Success(
+            TagsModel(listOf())
+        )
+        coEvery {
+            searchQueriesDataSource.getLastQueries()
+        } returns Resource.Success(listOf())
+
+        // when
+        val sut = createSut()
+        val response = sut.getSearchLandingData().first()
+
+        // then
+        assert(response is Resource.Error)
+    }
+
+    @Test
+    fun `getSearchLandingData - when tags is KO return Error`() = runBlocking {
+        // given
+        coEvery {
+            ingredientDataSource.getIngredientsList()
+        } returns Resource.Success(
+            IngredientsModel(listOf())
+        )
+        coEvery {
+            searchDataSource.getTags()
+        } returns Resource.Error(Exception())
+        coEvery {
+            searchQueriesDataSource.getLastQueries()
+        } returns Resource.Success(listOf())
+
+        // when
+        val sut = createSut()
+        val response = sut.getSearchLandingData().first()
+
+        // then
+        assert(response is Resource.Error)
+    }
+
+    @Test
+    fun `submitFilter - returns Success`() = runBlocking {
         // given
         coEvery { searchDataSource.queryFilter(any()) } returns Resource.Success(
             MainModel(drinks = listOf())
@@ -74,6 +154,19 @@ class SearchRepositoryImplTest {
 
         // then
         assert(response is Resource.Success<MainModel>)
+    }
+
+    @Test
+    fun `addQueryToLocalDatabase - should call data source method`() = runBlocking{
+        // given
+        val query = "testQuery"
+
+        // when
+        val sut = createSut()
+        sut.addQueryToLocalDatabase(query)
+
+        // then
+        coVerify { searchQueriesDataSource.insertQuery(query) }
     }
 
     private fun createSut() =
